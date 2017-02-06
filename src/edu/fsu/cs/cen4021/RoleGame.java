9726eeed602e9c2d74df9c7d8094d3e557bf32f8
David <dmr06d@my.fsu.edu>
2017-02-06 03:30:43 -0500

    Implemented new weapons in RoleGame
    Added tests for BasicArrow and BasicAxe weapons in RoleGame.java, based off enemy with 20 armor.

--- src/edu/fsu/cs/cen4021/RoleGame.java
+++ src/edu/fsu/cs/cen4021/RoleGame.java
@@ -14,6 +14,17 @@ public class RoleGame {
         int armor = 20;
         System.out.println("Swords was able to do " + sword.hit(armor) + " of damage due to an armor with " + armor + " points.");
 
+        Weapon arrow = WeaponFactory.getWeapon("basicarrow");
+        System.out.println("BasicArrow has " + arrow.hit() + " of damage.");
+        int armor = 20;
+        System.out.println("BasicArrow was able to do " + arrow.hit(armor) + " of damage due to an armor with " + armor + " points.");
+
+        Weapon axe = WeaponFactory.getWeapon("basicaxe");
+        System.out.println("BasicAxe has " + axe.hit() + " of damage.");
+        int armor = 20;
+        System.out.println("Swords was able to do " + axe.hit(armor) + " of damage due to an armor with " + armor + " points.");
+
+
         //TODO: Add the remaining weapons here
 
     }
